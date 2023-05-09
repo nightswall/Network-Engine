e86e692c9aef775a714c838578a598b6991df460
TUGRAHAN GORKEM TURKMEN <e2337038@ceng.metu.edu.tr>
2023-05-09 03:43:08 +0300

    b

--- prediction_tester.py
+++ prediction_tester.py
@@ -25,6 +25,7 @@ def test_model(checkpoint_path, test_set):
         df = pd.read_csv(testing_path, skiprows=idx+1, nrows=1)
         print(f"In chunk {idx} with Test Set: {test_set}")
 
+        print(df.shape)
         df = df.columns.drop("target")
         df = np.expand_dims(df, axis=0)
         print(df.shape)
