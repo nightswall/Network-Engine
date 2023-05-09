110d760519f20397aa881d79d1387709fb215032
TUGRAHAN GORKEM TURKMEN <e2337038@ceng.metu.edu.tr>
2023-05-09 03:57:14 +0300

    b

--- prediction_tester.py
+++ prediction_tester.py
@@ -40,6 +40,7 @@ def test_model(checkpoint_path, test_set):
         del df[df.columns[-1]]
         x_testing = df[df.columns].values
         x_testing[0][0] = fl(int(x_testing[0][0], 16)).value
+        x_testing[1][0] = fl(int(x_testing[1][0], 16)).value
         x_testing = np.asarray(x_testing).astype('float32')
 
         detector, _ = model.create_model(x_testing.shape[1], checkpoint_path)
