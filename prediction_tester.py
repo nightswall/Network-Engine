489e1cede7c390fcc3fdf0e44b613078398f1076
TUGRAHAN GORKEM TURKMEN <e2337038@ceng.metu.edu.tr>
2023-05-09 04:15:42 +0300

    b

--- prediction_tester.py
+++ prediction_tester.py
@@ -14,19 +14,6 @@ data_loader = DataLoader()
 results = []
 model_checkpoints = ["checkpoints/ckpt70_reduced.csv/cp70_reduced.csv.ckpt", "checkpoints/ckpt70/cp70.ckpt"]
 test_sets = ["test30", "test30_reduced", "test30_augmented"]
-CHUNK_SIZE = 10 ** 6
-
-class fl:
-    def __init__(this, value=0, byte_size=4):
-        this.value = value
-        if this.value: # speedy check (before performing any calculations)
-            Fe=((byte_size*8)-1)//(byte_size+1)+(byte_size>2)*byte_size//2+(byte_size==3)
-            Fm,Fb,Fie=(((byte_size*8)-(1+Fe)), ~(~0<<Fe-1), (1<<Fe)-1)
-            FS,FE,FM=((this.value>>((byte_size*8)-1))&1,(this.value>>Fm)&Fie,this.value&~(~0 << Fm))
-            if FE == Fie: this.value=(float('NaN') if FM!=0 else (float('+inf') if FS else float('-inf')))
-            else: this.value=((pow(-1,FS)*(2**(FE-Fb-Fm)*((1<<Fm)+FM))) if FE else pow(-1,FS)*(2**(1-Fb-Fm)*FM))
-            del Fe; del Fm; del Fb; del Fie; del FS; del FE; del FM
-        else: this.value = 0.0
 
 def test_model(checkpoint_path, test_set):
     global results
@@ -45,7 +32,6 @@ def test_model(checkpoint_path, test_set):
 
         del df[df.columns[-1]]
         x_testing = df[df.columns].values
-        print(x_testing)
 
         print(f"In chunk {idx} with Test Set: {test_set}")
         print(get_prediction(detector, x_testing))
