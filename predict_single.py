647be6edae118042998f3c9e3c738118abe720b1
TUGRAHAN GORKEM TURKMEN <e2337038@ceng.metu.edu.tr>
2023-05-09 03:16:15 +0300

    Working version

--- predict_single.py
+++ predict_single.py
@@ -11,10 +11,16 @@ flow_types = {0: "bruteforce",
 
 def get_prediction(model = None, incoming_message = None):
 	global flow_types
-	if model:
+	if model is not None and incoming_message is not None:
 		# Prediction from the engine
 		prediction = model.predict(incoming_message)
 		prediction = np.argmax(prediction, axis = 1)
 
-		print(prediction)
-		print(predictionReduced)
+		result = dict()
+
+		if prediction[0] != 2:
+			result = {"type": "MALICIOUS", "prediction": flow_types[prediction[0]]}
+		else:
+			result = {"type": "LEGITIMATE", "prediction": flow_types[prediction[0]]}
+
+		return result
