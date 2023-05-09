37091abf15adb08131180876fa0f2a57cd92813c
TUGRAHAN GORKEM TURKMEN <e2337038@ceng.metu.edu.tr>
2023-05-09 04:17:01 +0300

    a

--- predict_single.py
+++ predict_single.py
@@ -14,7 +14,6 @@ def get_prediction(model = None, incoming_message = None):
 	if model is not None and incoming_message is not None:
 		# Prediction from the engine
 		prediction = model.predict(incoming_message, batch_size = 1)
-		prediction = np.argmax(prediction, axis = 1)
 
 		print(prediction)
 
