136597f9ce21126e4dfc641791bb3ef86607ae8b
TUGRAHAN GORKEM TURKMEN <e2337038@ceng.metu.edu.tr>
2023-05-30 13:05:48 +0300

    Predict Single script file is adapted to the Django API. The get_prediction() function now takes single parameter, the request and returns a HtmlResponse. The results of the testing period are moved to the folder results. Related to: https://gitlab.ceng.metu.edu.tr/group-17/diona/-/issues/107

--- predict_single.py
+++ predict_single.py
@@ -3,14 +3,20 @@ import numpy as np
 import json
 
 flow_types = {0: "BruteForce", 1: "dos", 3: "legitimate", 4: "malformed", 2: "SlowITe", 5: "Flooding"}
+model_checkpoint = "checkpoints/ckpt70_reduced/ckpt70_reduced.ckpt"
+session_checkpoint = "checkpoints/ckpt70_reduced/session.ckpt"
 
-def get_prediction(model = None, incoming_message = None):
-	global flow_types
-	if model is not None and incoming_message is not None:
-		# Prediction from the engine
-		prediction = model.predict(incoming_message)
+
+def get_prediction(incoming_message = None):
+	global flow_types, model_checkpoint, session_checkpoint
+	model, _ = Model().create_model(33, model_checkpoint) # Creating a new model for reference
+	model = model.load_model(model, model_checkpoint, session_checkpoint) # Loading model checkpoint and session
+
+	print(incoming_message)
+
+	if incoming_message is not None: # Checking if a valid request is made
+		prediction = model.predict(incoming_message) # Prediction from the engine
 		prediction = np.argmax(prediction, axis = 1)
-		#print(flow_types[prediction[0]])
 		result = []
 
 		for p in prediction:
@@ -21,6 +27,8 @@ def get_prediction(model = None, incoming_message = None):
 				f = {"type": "MALICIOUS", "prediction": flow_types[p]} 
 			result.append(f)
 
-		decisions = {"type": "RESPONSE", "predictions": result}
-		#print(decisions["predictions"])
-		return decisions
+		decisions = {"type": "RESPONSE", "predictions": result} # Decision
+		response = HttpResponse(json.dumps({
+				"prediction": decisions
+			}))
+		return response
