3de758df4411eeb7752e33c2bdf07f810ff13d1f
TUGRAHAN GORKEM TURKMEN <e2337038@newinek>
2023-06-04 16:59:51 +0300

    bugfix

--- data_loader.py
+++ data_loader.py
@@ -3,6 +3,9 @@ from warnings import simplefilter
 
 flow_types = {"bruteforce": 0, "dos": 1, "legitimate": 2, "malformed": 3, "slowite": 4, "flooding": 5}
 
+# The flow types, namely classes are now static since Pandas randomize class codes at every instance
+# it loads. So, they are made static to make them same throughout the whole project.
+
 class DataLoader():
     def initalize_training_data(__self__, dataset_path):
         training_set = pd.read_csv(dataset_path)
@@ -11,15 +14,18 @@ class DataLoader():
         training_set = training_set.astype("category")
         category_columns = training_set.select_dtypes(["category"]).columns
 
-        for category in category_columns:
-            if category != "target":
-                testing_set[category] = testing_set[category].apply(lambda x : x.cat.codes)
+        # for category in category_columns:
+        #     if category != "target":
+        #         testing_set[category] = testing_set[category].apply(lambda x : x.cat.codes)
 
+        training_set["tcp.flags"] = training_set["tcp.flags"].apply(lambda x : float(int(str(x), 16)))
+        training_set["mqtt.conflags"] = training_set["mqtt.conflags"].apply(lambda x : float(int(str(x), 16)))
+        training_set["mqtt.hdrflags"] = training_set["mqtt.hdrflags"].apply(lambda x : float(int(str(x), 16)))
         
         #testing_set["tcp.flags"] = testing_set["tcp.flags"].apply(lambda x : float(int(str(x), 16)))
         #testing_set["mqtt.conflags"] = testing_set["mqtt.conflags"].apply(lambda x : float(int(str(x), 16)))
         #testing_set["mqtt.hdrflags"] = testing_set["mqtt.hdrflags"].apply(lambda x : float(int(str(x), 16)))
-        testing_set["target"] = testing_set["target"].apply(lambda x : flow_types[x])
+        training_set["target"] = training_set["target"].apply(lambda x : flow_types[x])
 
 
         #training_set[category_columns] = training_set[category_columns].apply(lambda x : x.cat.codes)
@@ -35,13 +41,13 @@ class DataLoader():
         testing_set = testing_set.astype("category")
         category_columns = testing_set.select_dtypes(["category"]).columns
 
-        for category in category_columns:
-            if category != "target":
-                testing_set[category] = testing_set[category].apply(lambda x : x.cat.codes)
+        # for category in category_columns:
+        #     if category != "target":
+        #         testing_set[category] = testing_set[category].apply(lambda x : x.cat.codes)
 
-        #testing_set["tcp.flags"] = testing_set["tcp.flags"].apply(lambda x : float(int(str(x), 16)))
-        #testing_set["mqtt.conflags"] = testing_set["mqtt.conflags"].apply(lambda x : float(int(str(x), 16)))
-        #testing_set["mqtt.hdrflags"] = testing_set["mqtt.hdrflags"].apply(lambda x : float(int(str(x), 16)))
+        testing_set["tcp.flags"] = testing_set["tcp.flags"].apply(lambda x : float(int(str(x), 16)))
+        testing_set["mqtt.conflags"] = testing_set["mqtt.conflags"].apply(lambda x : float(int(str(x), 16)))
+        testing_set["mqtt.hdrflags"] = testing_set["mqtt.hdrflags"].apply(lambda x : float(int(str(x), 16)))
         testing_set["target"] = testing_set["target"].apply(lambda x : flow_types[x])
 
         #testing_set[category_columns] = testing_set[category_columns].apply(lambda x : x.cat.codes)
